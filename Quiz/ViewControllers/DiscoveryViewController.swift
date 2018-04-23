163227ba7222d827e086dd462766166a7525b26b
hemant3370 <hrajput4@gmail.com>
2018-04-23 09:56:51 +0530

    Multiplayer support

---
+++ Quiz/ViewControllers/DiscoveryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DiscoveryViewController.swift
+//  Quiz
+//
+//  Created by Hemant Singh on 18/04/18.
+//  Copyright © 2018 Hemant. All rights reserved.
+//
+
+import UIKit
+
+class DiscoveryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
